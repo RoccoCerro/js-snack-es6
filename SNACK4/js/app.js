d3dc437f78dad5e603dc8ea7d006361dd669b2b3
RoccoCerro <rcerro02@gmail.com>
2024-03-20 15:12:07 +0100

    Add: punti e falli subiti random

--- SNACK4/js/app.js
+++ SNACK4/js/app.js
@@ -1,1 +1,48 @@
 console.log("ciao");
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+  
+
+const club = [
+    {
+        nome: 'Milan',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Roma',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Lazio',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Sassuolo',
+        punti: 0,
+        falliSubiti: 0
+    },
+]
+
+club.forEach((el)=>{
+    el.punti = getRandomIntInclusive(10,120);
+    el.falliSubiti = getRandomIntInclusive(0,30)
+})
+
+console.log(club)
